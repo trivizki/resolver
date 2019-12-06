a101f063414738e0f47509ee3eb2d074761684d6
Amit Trivizki <amit.trivizki@gmail.com>
2019-12-06 19:34:09 +0200

    Fix Bug, sniffer stopped when got error from network interface handler. Now, sniffer continue to listen after error.

--- sniffer/sniffer.go
+++ sniffer/sniffer.go
@@ -65,7 +65,7 @@ func (s *Sniffer) Sniff(){
 		packet, err := packetSource.NextPacket()
 		if err != nil {
 			fmt.Printf("ERROR!! %s\n", err.Error())
-			break
+			continue	
 		}
 		s.packetChannel <- packet
 	}
