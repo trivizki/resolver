5f5299dae6cf04e77ef4b0d849258140d5127e0e
omermoses <omermoses17@gmail.com>
2019-12-15 12:29:15 -0800

    again

--- tracking/omer-DB.go
+++ tracking/omer-DB.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-
+	MAX_CAPACITY int = 1000
 )
 
 		
@@ -16,15 +16,19 @@ type myDB struct{
 
 // 
 type DBTracker struct {
-	*MyDBTbleByTime [][2]string
-	*MyDBTableByName [][2]string
+	myDBTbleByTime *[][]string
+	myDBTableByName *[][]string
 	logger *logger.Logger
 }
 	
 //create new DBtracker object.
 func NewDBTracker(logger *logger.Logger) *DBTracker{
-	*MyDBTableByName = make([][2]string, )
-	*MyDBTableByTime = make([][2]string, )
+	return &DBTracker{
+		myDBTableByName : make([][]string, 0, MAX_CAPACITY)
+		myDBTbleByTime : make([][]string,0, MAX_CAPACITY)
+		logger: logger
+	}
+
 }
 
 // Initialzie the Tracker object, i.e. connecting to DB
@@ -39,35 +43,41 @@ func (st *DBTracker) InitializeTracker() error {
 func (st *DBTracker ) RecordDomain(name string, timestamp time.Time) error {
 		
 	//updtae the time table - the time table arranged by time of arrival(AKA TOA)
-	*MyDBTableBytime = append(MyDBTableByName, {timestamp, name})
+	*myDBTableBytime = append(myDBTableBytime, timestamp)
+	*myDBTableBytime[len(myDBTbleByTime-1)][2] = name
 		
 	//updae the name table - the name table shall be orginaized lexi...
-	MyDBTableByName
-
+	myDBTableByName = append(myDBTableBytime, name)
+	myDBTableByName [len(myDBTableByName)-1][timestamp] 
+    sort.Slice(myDBTableByName)
 	}		
 
 	// Gets recorded domains that occured more than the given amount of times.
 	func (st *MyDBTableByName)GetDomainsByAmount(amount int) ([]string, error){
 		
-		var result string[]
+		result := map[string]int
+
+		for index name := range myDBTableByName{
+			if value > 0, := dict[name]; {
+				result[name] += 1
+			} else {
+				result[name] = 1
+			}
+
+		}
+
+
 
 	}
 
 	// Get domains that their last query was before the given date.
-	GetOldDomainsByDate(date time.Time)([]string, error)
+	func (st *DBTracker)GetOldDomainsByDate(date time.Time)([]string, error){
+
+
+	}
 
 	// Delete from table the given domain.
 
-	func (st *MYSQLTracker) DeleteDomainByName(name string) error{
-		func SearchStrings(a []string, name string) int
-}
-
-// Initiazlies the MYSQLTracker object. connect to the relevant MySql DB.
-// NOTICE: user must call this function before using the Tracker.
-
+	func (st *DBTracker) DeleteDomainByName(name string) error{
 	
-func (st *MYSQLTracker) GetOldDomainsByDate(date time.Time)([]string, error){
-		
-
-
 	}
